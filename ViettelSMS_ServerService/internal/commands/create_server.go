292939f869a7c1fceacb557994945352ca9cbd31
th1enq <thienchy3305@gmail.com>
2025-08-05 10:04:22 +0700

    init project

--- ViettelSMS_ServerService/internal/commands/create_server.go
+++ ViettelSMS_ServerService/internal/commands/create_server.go
@@ -1,7 +1,6 @@
 package commands
 
 import (
-	"ViettelSMS_ServerService/internal/domain"
 	"ViettelSMS_ServerService/pkg/es"
 	"context"
 
@@ -36,33 +35,4 @@ func NewCreateServerCmdHandler(logger *zap.Logger, aggregateStore es.AggregateSt
 }
 
 func (c *createServerCmdHandler) Handle(ctx context.Context, cmd CreateServerCommand) error {
-	c.logger.Info("Handling CreateServer command", zap.Any("command", cmd))
-
-	exists, err := c.aggregateStore.Exists(ctx, cmd.AggregateID)
-	if err != nil {
-		c.logger.Error("Error checking if aggregate exists", zap.Error(err))
-		return err
-	}
-	if exists {
-		c.logger.Warn("Aggregate already exists", zap.String("aggregate_id", cmd.AggregateID))
-		return nil
-	}
-
-	// Create the aggregate
-	serverAggregate := domain.NewServerAggregate(cmd.AggregateID)
-	err = serverAggregate.CreateNewServer(
-		ctx,
-		cmd.ServerID,
-		cmd.ServerName,
-		cmd.IPv4,
-		cmd.Location,
-		cmd.OS,
-		cmd.IntervalTime,
-	)
-	if err != nil {
-		c.logger.Error("Error creating new server", zap.Error(err))
-		return err
-	}
-
-	return c.aggregateStore.Save(ctx, serverAggregate)
 }
